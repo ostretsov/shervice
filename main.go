f62db316a1c48b5de1e6983006e86d6ce0bbd361
Artem Ostretsov <OstretsovAA@gmail.com>
2017-01-03 10:34:15 +0300

    +http server

--- main.go
+++ main.go
@@ -7,29 +7,40 @@ import (
 	"net/http"
 )
 
+func loadConfig(configFile string) (string, error) {
+	configData, err := ioutil.ReadFile(configFile)
+	if nil != err {
+		return "", err
+	}
+	appConfig, err := config.ParseYaml(string(configData))
+	if nil != err {
+		return "", err
+	}
+	value, err := appConfig.String("services.0.url")
+	if nil != err {
+		return "", err
+	}
+
+	return value, err;
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "echo is called: %s", r.URL.Path[1:])
 }
 
 func startServer() {
 	http.HandleFunc("/echo", handler)
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if nil != err {
+		fmt.Println(err)
+	}
 }
 
 func main() {
 	configFile := "./config/shervice.yaml"
-	configData, err := ioutil.ReadFile(configFile)
+	value, err := loadConfig(configFile)
 	if nil != err {
-		fmt.Printf("Could not open file %s", configFile)
-	}
-	appConfig, err := config.ParseYaml(string(configData))
-	if nil != err {
-		fmt.Printf(err.Error())
-	}
-
-	value, err := appConfig.String("services.0.url")
-	if nil != err {
-		fmt.Println("Could not find a service!")
+		fmt.Println(err)
 	}
 
 	fmt.Println(value)
